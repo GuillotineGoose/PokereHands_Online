2f6be7e4d14bf791af77d13bcef44aaa1fdb6055
RobinFlaks <robin_flaks@hotmail.com>
2022-10-15 14:19:22 +0200

    🔴test drawACard

--- src/test/java/Card/DeckTest.java
+++ src/test/java/Card/DeckTest.java
@@ -1,11 +1,17 @@
 package Card;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeckTest {
     private Deck deck;
+
+    @BeforeEach
+    public void initTest(){
+        deck = new Deck();
+    }
 
     @Test
     public void drawACard(){
@@ -14,12 +20,12 @@ public class DeckTest {
         assertEquals(testCard.getRank(), drawnCard.getRank());
     }
 
-    @Test void drawWholeDeck(){
+    /*@Test void drawWholeDeck(){
 
     }
 
     @Test
     public void drawToManyCards(){
 
-    }
+    }*/
 }

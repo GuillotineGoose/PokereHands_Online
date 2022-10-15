6fc9ef679c1a597beb86871ade70cf2451857fdb
GuillotineGoose <haavar.c.karlsen@gmail.com>
2022-10-15 14:18:18 +0200

    🟢 getRankTest & getSuitTest * asserNotEqual rank * assertNotEqual suit

--- src/test/java/Card/CardTest.java
+++ src/test/java/Card/CardTest.java
@@ -2,18 +2,21 @@ package Card;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class    CardTest {
     @Test
     public void getRankTest(){
         var card = new Card(Rank.TEN, Suit.DIAMONDS);
         assertEquals(Rank.TEN, card.getRank());
+        assertNotEquals(Rank.FIVE, card.getRank());
     }
 
     @Test
     public void getSuitTest(){
         var card = new Card(Rank.TEN, Suit.DIAMONDS);
         assertEquals(Suit.DIAMONDS, card.getSuit());
+        assertNotEquals(Suit.HEARTS, card.getSuit());
+
     }
 }

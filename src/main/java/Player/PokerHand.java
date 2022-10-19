320a5d320885bb3c92500dfee632af139dbff186
GuillotineGoose <Haavar.c.karlsen@gmail.com>
2022-10-19 14:54:29 +0200

    🔴 checkHighCard (Returns Null)

--- src/main/java/Player/PokerHand.java
+++ src/main/java/Player/PokerHand.java
@@ -1,14 +1,64 @@
 package Player;
 
-public enum PokerHand {
-    HIGH_CARD,
-    ONE_PAIR,
-    TWO_PAIR,
-    THREE_OF_A_KIND,
-    STRAIGHT,
-    FLUSH,
-    FULL_HOUSE,
-    FOUR_OF_A_KIND,
-    STRAIGHT_FLUSH,
-    ROYAL_FLUSH
+import Card.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PokerHand {
+    private List<Card> hand;
+
+    private PokerHands score;
+    private Rank highCard;
+
+    private Map<Rank, List<Card>> handValues;
+
+    public PokerHand(){
+        hand = new ArrayList<>();
+        handValues = new HashMap<>();
+    }
+
+    public void addCard(Card card){
+        hand.add(card);
+    }
+
+
+    public void checkIfStraightFlush(){
+
+    }
+    public void checkIfFourOfAKind(){
+
+    }
+    public void checkIfFullHouse(){
+
+    }
+    public void checkIfFlush(){
+
+    }
+    public void checkIfStraight(){
+
+    }
+    public void checkIfThreeOfAKind(){
+
+    }
+    public void checkIfTwoPairs(){
+
+    }
+    public void checkIfPair(){
+
+    }
+
+    public void checkHighCard(){
+
+    }
+
+    public PokerHands getScore() {
+        return null;
+    }
+
+    public Rank getHighCard() {
+        return null;
+    }
 }
--- src/test/java/Player/PokerScoreTest.java
+++ src/test/java/Player/PokerScoreTest.java
@@ -1,7 +1,31 @@
 package Player;
 
-import static org.junit.jupiter.api.Assertions.*;
+import Card.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PokerScoreTest {
+    private PokerHand player;
 
+    @BeforeEach
+    void setUp() {
+        player = new PokerHand();
+    }
+
+    @Test
+    void checkHighCard(){
+
+        player.addCard(new Card(Rank.FIVE, Suit.CLUBS));
+        player.addCard(new Card(Rank.J, Suit.SPADES));
+        player.addCard(new Card(Rank.TEN, Suit.SPADES));
+        player.addCard(new Card(Rank.K, Suit.HEARTS));
+        player.addCard(new Card(Rank.A, Suit.CLUBS));
+
+        assertEquals(PokerHands.HIGH_CARD, player.getScore());
+        assertEquals(Rank.K, player.getHighCard());
+
+
+    }
 }

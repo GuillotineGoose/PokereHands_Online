a0408c28f6f90a3c0658f8b90e9e7ef115720d58
GuillotineGoose <Haavar.c.karlsen@gmail.com>
2022-10-19 13:03:34 +0200

    Score and Hand added. * Hand is a representation of a player and may change name later

---
+++ src/main/java/Player/Hand.java
@@ -0,0 +1,11 @@
+package Player;
+
+import Card.Card;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Hand {
+    List<Card> hand; //This will be the two cards unique to the player
+
+}
--- src/main/java/Player/PokerScore.java
+++ src/main/java/Player/PokerScore.java
@@ -1,4 +1,13 @@
 package Player;
 
+import Card.Card;
+
+import java.util.List;
+
 public class PokerScore {
+
+
+    public Score checkHighCard(){
+
+    }
 }
